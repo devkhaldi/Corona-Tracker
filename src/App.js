0a5e3ea774b88903e301ab23fc7fd90fe889da6e
devkhaldi <dev.khaldi@gmail.com>
2020-05-13 23:55:27 +0000

    Get data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import styles from './App.module.css'
 const apiUrl = 'https://covid19.mathdro.id/api'
 
 function App() {
-  const [data, setData] = useState({ confirmed: {}, recovered: {}, deaths: {} })
+  const [data, setData] = useState({
+    confirmed: {},
+    recovered: {},
+    deaths: {},
+    lastUpdate: {},
+  })
   useEffect(() => {
     axios
       .get(apiUrl)
-      .then(({ data: { confirmed, recovered, deaths } }) =>
-        setData({ confirmed, recovered, deaths })
+      .then(({ data: { confirmed, recovered, deaths, lastUpdate } }) =>
+        setData({ confirmed, recovered, deaths, lastUpdate })
       )
       .catch(error => console.log(error))
   }, [])
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
 import styles from './Cards.module.css'
 
-const Cards = props => {
-  console.log(props.data)
+const Cards = ({ data }) => {
+  console.log(data)
+  if (!data) return 'Loading ...'
+  const { confirmed, deaths, recovered, lastUpdate } = data
   return (
     <div className={styles.container}>
       <Grid container spaicing={3} justify='center'>
@@ -11,8 +13,8 @@ const Cards = props => {
           <Typography color='textSecondary' gutterBottom>
             Infected
           </Typography>
-          <Typography variant='h5'>Real data</Typography>
-          <Typography color='textSecondary'>Real Date</Typography>
+          <Typography variant='h5'>{confirmed.value}</Typography>
+          <Typography color='textSecondary'></Typography>
           <Typography varient='body2'>Number of active cases</Typography>
         </Grid>
 
