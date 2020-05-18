faabf0f2fbc7fbde8b570a3dea20a78853b46df8
devkhaldi <dev.khaldi@gmail.com>
2020-05-18 23:14:59 +0000

    Country Selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
+import axios from 'axios'
+
+const apiUrl = 'https://covid19.mathdro.id/api/countries'
 
 function App() {
+  const [selectedCountry, setSelectedCountry] = useState('global')
+  const [countryData, setCountryData] = useState({})
+
+  useEffect(() => {
+    if (selectedCountry !== 'global') {
+      console.log(selectedCountry)
+      axios
+        .get(`${apiUrl}/${selectedCountry}`)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+    }
+  }, [selectedCountry])
   return (
     <div className={styles.container}>
       <Cards />
-      <CountryPicker />
-      <Chart />
+      <CountryPicker setSelectedCountry={setSelectedCountry} />
+      <Chart countryData={countryData} />
     </div>
   )
 }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import cx from 'classnames'
+import styles from './CountryPicker.module.css'
 
-const CountryPicker = () => {
-  return <div>CountryPicker</div>
+const apiUrl = 'https://covid19.mathdro.id/api/countries'
+
+const CountryPicker = ({ setSelectedCountry }) => {
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState('Global')
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then(res => setCountries(res.data.countries))
+      .catch(error => console.log(error))
+  }, [country])
+
+  const handelChange = e => {
+    setSelectedCountry(e.target.value)
+    setCountry(e.target.value)
+  }
+  return (
+    <form className={cx(styles.container, 'my-3')}>
+      <select onChange={handelChange} value='global' className='form-control'>
+        <option value={country}>{country}</option>
+        {countries.map(country => (
+          <option key={country.iso3} value={country.name}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+    </form>
+  )
 }
 
 export default CountryPicker
